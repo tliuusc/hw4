eaa596151871ba3f89187e7db05fbb902bccd246
Terry Liu <terry.liu@kellogg.northwestern.edu>
2023-05-26 19:50:19 +0000

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+     # authenticate the user
+     @user = User.find_by({ "email" => params["email"] })
+     if @user
+       if BCrypt::Password.new(@user["password"]) == params["password"]
+         session["user_id"] = @user["id"]
+         flash["notice"] = "Welcome, #{@user["first_name"]}."
+         redirect_to "/places"
+       else
+         flash["notice"] = "Incorrect email/password combination."
+         redirect_to "/login"
+       end
+     else
+       flash["notice"] = "Incorrect email/password combination."
+       redirect_to "/login"
+     end
   end
 
   def destroy
+    flash["notice"] = "You are now logged out."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get "/", :controller => "places", :action => "index"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+  
+  get "/", :controller => "places", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+
 end
